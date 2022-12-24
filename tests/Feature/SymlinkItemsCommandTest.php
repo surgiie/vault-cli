68cd273836a58f608869945bfd41102e52777267
Sergio Compean <scompean24@gmail.com>
2022-12-23 23:41:25 -0500

    test

--- tests/Feature/SymlinkItemsCommandTest.php
+++ tests/Feature/SymlinkItemsCommandTest.php
@@ -56,14 +56,44 @@ it('can symlink items with permissions', function () {
 
     $symlink_one = $test_vault_path.'/test-links/example';
     mkdir(dirname($symlink_one));
-    $symlink_two = $test_vault_path.'/test-links/example_two';
 
     $this->artisan('symlink', [
         '--link' => ['example:'.$symlink_one],
         '--password' => 'secret',
         '--user' => $user,
         '--group' => $user,
-        '--permissions' => '0777',
+        '--permissions' => '777',
+        '--force' => true,
+        '--vault-path' => $test_vault_path,
+    ])->assertExitCode(0);
+
+    expect(is_link($symlink_one))->toBeTrue();
+    expect(substr(decoct(fileperms($symlink_one)), -4))->toBe('0777');
+});
+
+it('can symlink items with permissions from item data', function () {
+    fresh_test_vault('local');
+
+    $test_vault_path = base_path('tests/vault');
+
+    $user = get_current_user();
+
+    $this->artisan('item:new', [
+        '--name' => 'example',
+        '--password' => 'secret',
+        '--content' => 'test',
+        '--vault-symlink-user' => $user,
+        '--vault-symlink-group' => $user,
+        '--vault-symlink-permissions' => '777',
+        '--vault-path' => $test_vault_path,
+    ])->assertExitCode(0);
+
+    $symlink_one = $test_vault_path.'/test-links/example';
+    mkdir(dirname($symlink_one));
+
+    $this->artisan('symlink', [
+        '--link' => ['example:'.$symlink_one],
+        '--password' => 'secret',
         '--force' => true,
         '--vault-path' => $test_vault_path,
     ])->assertExitCode(0);
