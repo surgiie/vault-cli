9000784a445998e8870a09aa5c6c7c73ac0343d3
Sergio Compean <scompean24@gmail.com>
2022-12-23 23:19:17 -0500

    tests

--- tests/Feature/SymlinkItemsCommandTest.php
+++ tests/Feature/SymlinkItemsCommandTest.php
@@ -39,3 +39,35 @@ it('can symlink items', function () {
     expect(is_link($symlink_one))->toBeTrue();
     expect(is_link($symlink_two))->toBeTrue();
 });
+
+it('can symlink items with permissions', function () {
+    fresh_test_vault('local');
+
+    $test_vault_path = base_path('tests/vault');
+
+    $user = get_current_user();
+
+    $this->artisan('item:new', [
+        '--name' => 'example',
+        '--password' => 'secret',
+        '--content' => 'test',
+        '--vault-path' => $test_vault_path,
+    ])->assertExitCode(0);
+
+    $symlink_one = $test_vault_path.'/test-links/example';
+    mkdir(dirname($symlink_one));
+    $symlink_two = $test_vault_path.'/test-links/example_two';
+
+    $this->artisan('symlink', [
+        '--link' => ['example:'.$symlink_one],
+        '--password' => 'secret',
+        '--user' => $user,
+        '--group' => $user,
+        '--permissions' => '0777',
+        '--force' => true,
+        '--vault-path' => $test_vault_path,
+    ])->assertExitCode(0);
+
+    expect(is_link($symlink_one))->toBeTrue();
+    expect(substr(decoct(fileperms($symlink_one)), -4))->toBe('0777');
+});
