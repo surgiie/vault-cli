4137d2347179cba1dfcecf0a53ada7bcff1ecc1f
Sergio Compean <scompean24@gmail.com>
2023-01-03 18:53:33 -0500

    Generate new version v0.11.0

--- app/Commands/NewVaultCommand.php
+++ app/Commands/NewVaultCommand.php
@@ -50,7 +50,6 @@ class NewVaultCommand extends BaseCommand
         }
 
         if (is_file($driverFilePath)) {
-            $this->message("TEST", "SUCKA");
             $this->exit('This vault already exists.');
         }
 
